46be643aa1fa16ba30a0089149c67c032a7a981c
Tarun Garg <tarungarg1208@hotmail.com>
2021-05-09 21:19:01 +0530

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  app_name = 'S-Shoppe';
+  constructor(private router: Router) {
+
+  }
+  setActive(value: string) {
+    console.log(this.router.url)
+    if (this.router.url.indexOf(value) > 0) {
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
+}
